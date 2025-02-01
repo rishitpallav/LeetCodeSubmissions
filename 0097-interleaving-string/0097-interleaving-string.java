1f83d1be660fcd50521ddf0439c9f86d6683e4b0
Rishit Pallav <64432673+rishitpallav@users.noreply.github.com>
2025-02-01 17:35:29 -0600

    Time: 17 ms (5.06%), Space: 42.7 MB (16.59%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -20,10 +20,12 @@ class Solution {
         }
 
         if (s1P < s1.length() && s1.charAt(s1P) == s3.charAt(s1P + s2P) && check(s1, s2, s3, s1P+1, s2P, memo)) {
+            memo.put(currentPair, true);
             return true;
         }
 
         if (s2P < s2.length() && s2.charAt(s2P) == s3.charAt(s1P + s2P) && check(s1, s2, s3, s1P, s2P+1, memo)) {
+            memo.put(currentPair, true);
             return true;
         }
         memo.put(currentPair, false);
