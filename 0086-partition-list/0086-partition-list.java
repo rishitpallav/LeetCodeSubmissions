22ae8b17746cdfb60e4c9c396b591dd94dfca88c
Rishit Pallav <64432673+rishitpallav@users.noreply.github.com>
2025-02-03 23:27:35 -0600

    Time: 0 ms (100%), Space: 42.7 MB (6.26%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -11,19 +11,20 @@
 class Solution {
     public ListNode partition(ListNode head, int x) {
         ListNode lower = new ListNode(0);
-        ListNode greater = new ListNode(0, head);
+        ListNode greater = new ListNode(0);
 
         ListNode lowerPointer = lower;
         ListNode greaterPointer = greater;
 
-        while (greater != null && greater.next != null) {
-            if (greater.next.val < x) {
-                lower.next = greater.next;
+        while (head != null) {
+            if (head.val < x) {
+                lower.next = new ListNode(head.val);
                 lower = lower.next;
-                greater.next = greater.next.next;
             } else {
+                greater.next = new ListNode(head.val);
                 greater = greater.next;
             }
+            head = head.next;
         }
 
         lower.next = greaterPointer.next;
