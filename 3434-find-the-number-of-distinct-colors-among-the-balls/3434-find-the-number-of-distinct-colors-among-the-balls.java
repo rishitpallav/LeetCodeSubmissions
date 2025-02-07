0f9186c88ae7cafae2b07afb55efd5b44d8cba40
Rishit Pallav <64432673+rishitpallav@users.noreply.github.com>
2025-02-06 21:11:55 -0600

    Time: 41 ms (60.7%), Space: 97.2 MB (65.07%) - LeetHub

--- 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.java
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.java
@@ -8,10 +8,7 @@ class Solution {
 
         for (int i = 0; i < queries.length; i++ ) {
             if (ballColor.containsKey(queries[i][0])) {
-                // update the ball color
                 int previousColor = ballColor.get(queries[i][0]);
-                ballColor.put(queries[i][0], queries[i][1]);
-
                 // update the color counter
                 // decrement the previous color
                 colorCounter.put(previousColor, colorCounter.get(previousColor)-1);
@@ -19,23 +16,13 @@ class Solution {
                 if (colorCounter.get(previousColor) == 0) {
                     counter--;
                 }
-
-                // increment the new color counter
-                colorCounter.put(queries[i][1], colorCounter.getOrDefault(queries[i][1], 0)+1);
-                // update the counter
-                if (colorCounter.get(queries[i][1]) == 1) {
-                    counter++;
-                }
-            } else {
-                // add a new ball color
-                ballColor.put(queries[i][0], queries[i][1]);
-
-                // update color counter
-                colorCounter.put(queries[i][1], colorCounter.getOrDefault(queries[i][1], 0)+1);
-                // update the counter
-                if (colorCounter.get(queries[i][1]) == 1) {
-                    counter++;
-                }
+            }
+            ballColor.put(queries[i][0], queries[i][1]);
+            // update color counter
+            colorCounter.put(queries[i][1], colorCounter.getOrDefault(queries[i][1], 0)+1);
+            // update the counter
+            if (colorCounter.get(queries[i][1]) == 1) {
+                counter++;
             }
             result[i] = counter;
         }
