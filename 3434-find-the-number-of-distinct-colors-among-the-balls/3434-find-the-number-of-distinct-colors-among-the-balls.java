cafa512c409811ce3e8a4bf9e4e0535727859a57
Rishit Pallav <64432673+rishitpallav@users.noreply.github.com>
2025-02-06 21:19:19 -0600

    Time: 38 ms (90.39%), Space: 79.6 MB (95.2%) - LeetHub

--- 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.java
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.java
@@ -9,22 +9,16 @@ class Solution {
         for (int i = 0; i < queries.length; i++ ) {
             if (ballColor.containsKey(queries[i][0])) {
                 int previousColor = ballColor.get(queries[i][0]);
-                // update the color counter
-                // decrement the previous color
-                colorCounter.put(previousColor, colorCounter.get(previousColor)-1);
-                // update the counter
-                if (colorCounter.get(previousColor) == 0) {
-                    counter--;
+                int colorCount = colorCounter.get(previousColor)-1;
+                if (colorCount == 0) {
+                    colorCounter.remove(previousColor);
+                } else {
+                    colorCounter.put(previousColor, colorCount);
                 }
             }
             ballColor.put(queries[i][0], queries[i][1]);
-            // update color counter
             colorCounter.put(queries[i][1], colorCounter.getOrDefault(queries[i][1], 0)+1);
-            // update the counter
-            if (colorCounter.get(queries[i][1]) == 1) {
-                counter++;
-            }
-            result[i] = counter;
+            result[i] = colorCounter.size();
         }
         return result;
     }
