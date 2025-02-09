2130824c0f1cd17bbac18740cc3ec841fd12b57e
Rishit Pallav <64432673+rishitpallav@users.noreply.github.com>
2025-02-09 17:07:38 -0600

    Time: 4 ms (22.31%), Space: 44.9 MB (82.66%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.java
+++ 0529-minesweeper/0529-minesweeper.java
@@ -7,8 +7,6 @@ class Solution {
 
         Queue<int[]> clicks = new LinkedList<>();
         clicks.add(click);
-        Set<String> visited = new HashSet<>();
-        visited.add(click[0]+","+click[1]);
 
         int[][] directions = new int[][]{{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
 
@@ -21,23 +19,25 @@ class Solution {
                 int i = direction[0] + temp[0];
                 int j = direction[1] + temp[1];
 
-                if (i >= 0 && i < board.length && j >= 0 && j < board[0].length) {
-                    if (board[i][j] == 'M') {
-                        surroundingBombs++;
-                    } else if (board[i][j] == 'E'  && !visited.contains(i+","+j) && surroundingBombs == 0) {
-                        potentialCells.add(new int[]{i,j});
-                    }
+                if (i >= 0 && i < board.length && j >= 0 && j < board[0].length && board[i][j] == 'M') {
+                    surroundingBombs++;
                 }
             }
 
             if (surroundingBombs > 0) {
                 board[temp[0]][temp[1]] = (char) ('0' + surroundingBombs);
             } else {
-                for (int[] cell : potentialCells) {
-                    clicks.add(cell);
-                    visited.add(cell[0] + "," + cell[1]);
-                }
                 board[temp[0]][temp[1]] = 'B';
+
+                for (int[] direction : directions) {
+                    int i = direction[0] + temp[0];
+                    int j = direction[1] + temp[1];
+
+                    if (i >= 0 && i < board.length && j >= 0 && j < board[0].length && board[i][j] == 'E') {
+                        board[i][j] = 'B';
+                        clicks.add(new int[]{i,j});
+                    }
+                }
             }
         }
 
