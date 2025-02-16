0db26c6b0b3f011f76d9758102da6b1a8c3eac46
Rishit Pallav <64432673+rishitpallav@users.noreply.github.com>
2025-02-15 18:04:43 -0600

    Time: 138 ms (51.4%), Space: 45.1 MB (5.3%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.java
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.java
@@ -1,24 +1,30 @@
 class Solution {
     public int punishmentNumber(int n) {
         int total = 0;
-        for (int i = 1; i <= n; i++) {
-            if (matches(0, i, 0, Integer.toString(i * i))) {
-                total += i * i;
+
+        for (int i = 1; i <= n; i++ ) {
+            if (matches(0, i, 0, Integer.toString(i*i))) {
+                total += i*i;
             }
         }
+
         return total;
     }
 
-    boolean matches(int index, int target, int currentTotal, String currentString) {
-        if (index == currentString.length()) {
-            return currentTotal == target;
+    boolean matches(int i, int target, int currentTotal, String currentString) {
+        if (i == currentString.length()) {
+            if (target == currentTotal) {
+                return true;
+            }
+            return false;
         }
-        for (int j = index; j < currentString.length(); j++) {
-            int partVal = Integer.parseInt(currentString.substring(index, j + 1));
-            if (matches(j + 1, target, currentTotal + partVal, currentString)) {
+
+        for (int j = i; j < currentString.length(); j++ ) {
+            if (matches(j+1, target, currentTotal + Integer.parseInt(currentString.substring(i,j+1)), currentString)) {
                 return true;
             }
         }
+
         return false;
     }
 }
