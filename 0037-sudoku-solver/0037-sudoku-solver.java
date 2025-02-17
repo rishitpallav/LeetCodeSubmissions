0add64930500d78f7e89055e6059652467a69999
Rishit Pallav <64432673+rishitpallav@users.noreply.github.com>
2025-02-17 12:36:25 -0600

    Time: 80 ms (93.06%), Space: 40.8 MB (96.86%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,31 +1,21 @@
 class Solution {
-    Set<Character>[] rowSet;
-    Set<Character>[] colSet;
-    Set<Character>[][] matSet;
+    boolean[][] rowSet;
+    boolean[][] colSet;
+    boolean[][][] matSet;
 
     public void solveSudoku(char[][] board) {
         
-        rowSet = new HashSet[9];
-        colSet = new HashSet[9];
-        matSet = new HashSet[3][3];
+        rowSet = new boolean[9][10];
+        colSet = new boolean[9][10];
+        matSet = new boolean[3][3][10];
 
-        for (int i = 0; i < 9; i++ ) {
-            rowSet[i] = new HashSet<>();
-            colSet[i] = new HashSet<>();
-        }
-
-        for (int i = 0; i < 3; i++ ) {
-            for (int j = 0; j < 3; j++ ) {
-                matSet[i][j] = new HashSet<>();
-            }
-        }
-        
         for (int i = 0; i < 9; i++ ) {
             for (int j = 0; j < 9; j++ ) {
                 if (board[i][j] != '.') {
-                    rowSet[i].add(board[i][j]);
-                    colSet[j].add(board[i][j]);
-                    matSet[i/3][j/3].add(board[i][j]);
+                    int num = board[i][j] - '0';
+                    rowSet[i][num] = true;
+                    colSet[j][num] = true;
+                    matSet[i/3][j/3][num] = true;
                 }
             }
         }
@@ -37,19 +27,19 @@ class Solution {
         for (int row = 0; row < 9; row++ ) {
             for (int column = 0; column < 9; column++ ) {
                 if (board[row][column] == '.') {
-                    for (char i = '1'; i <= '9'; i++ ) {
-                        if (!rowSet[row].contains(i) && !colSet[column].contains(i) && !matSet[row/3][column/3].contains(i)) {
-                            board[row][column] = i;
-                            rowSet[row].add(i);
-                            colSet[column].add(i);
-                            matSet[row/3][column/3].add(i);
+                    for (char i = 1; i <= 9; i++ ) {
+                        if (!rowSet[row][i] && !colSet[column][i] && !matSet[row/3][column/3][i]) {
+                            board[row][column] = (char) (i+'0');
+                            rowSet[row][i] = true;
+                            colSet[column][i] = true;
+                            matSet[row/3][column/3][i] = true;
                             if (backtrack(board)) {
                                 return true;
                             }
                             board[row][column] = '.';
-                            rowSet[row].remove(i);
-                            colSet[column].remove(i);
-                            matSet[row/3][column/3].remove(i);
+                            rowSet[row][i] = false;
+                            colSet[column][i] = false;
+                            matSet[row/3][column/3][i] = false;
                         }
                     }
                     return false;
