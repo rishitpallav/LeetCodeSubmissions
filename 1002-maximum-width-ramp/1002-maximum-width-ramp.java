db1890b9db307dbc8ea6a747cd2f51b8ca5675fc
Rishit Pallav <64432673+rishitpallav@users.noreply.github.com>
2025-03-08 19:55:48 -0600

    Time: 27 ms (60.68%), Space: 55.5 MB (53.97%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.java
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.java
@@ -7,17 +7,12 @@ class Solution {
         for (int i = 0; i < nums.length; i++ ) {
             if (mono.isEmpty() || mono.peek()[0] > nums[i]) {
                 mono.push(new int[]{nums[i], i});
-            } else {
-                Stack<int[]> temp = new Stack<>();
+            }
+        }
 
-                while (!mono.isEmpty() && mono.peek()[0] <= nums[i]) {
-                    result = Math.max(result, i - mono.peek()[1]);
-                    temp.push(mono.pop());
-                }
-
-                while (!temp.isEmpty()) {
-                    mono.push(temp.pop());
-                }
+        for (int i = nums.length - 1 ; i > -1; i-- ) {
+            while (!mono.isEmpty() && mono.peek()[0] <= nums[i]) {
+                result = Math.max(result, (i - mono.pop()[1]));
             }
         }
 
