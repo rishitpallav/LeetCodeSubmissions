455a5e1a983f6b740fd9da3a2b64f035bd55b77f
Rishit Pallav <64432673+rishitpallav@users.noreply.github.com>
2025-03-11 11:27:35 -0500

    Time: 2 ms (87.46%), Space: 45.1 MB (38.53%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -7,8 +7,7 @@ class Solution {
     List<List<String>> result;
     
     public List<List<String>> solveNQueens(int n) {
-        row = new boolean[n+1];
-        col = new boolean[n+1];
+        col = new boolean[n];
         leftDiagonal = new boolean[n+n+1];
         rightDiagonal = new boolean[n+n+1];
 
@@ -36,12 +35,10 @@ class Solution {
         for (int col = 0; col < n; col++ ) {
             if (isValid(row, col, n)) {
                 previousJIndex[row] = col;
-                this.row[row] = true;
                 this.col[col] = true;
                 leftDiagonal[row + col] = true;
                 rightDiagonal[col - row + n] = true;
                 backtrack(row+1, n, previousJIndex);
-                this.row[row] = false;
                 this.col[col] = false;
                 leftDiagonal[row + col] = false;
                 rightDiagonal[col - row + n] = false;
@@ -50,7 +47,6 @@ class Solution {
     }
 
     boolean isValid(int i, int j, int n) {
-        if (row[i]) return false;
         if (col[j]) return false;
         if (leftDiagonal[i+j]) return false;
         if (rightDiagonal[j-i+n]) return false;
