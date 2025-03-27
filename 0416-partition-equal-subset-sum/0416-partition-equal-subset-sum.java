d13181132ea9467d773a1f1fc25c17805468d541
Rishit Pallav <64432673+rishitpallav@users.noreply.github.com>
2025-03-27 09:28:47 -0500

    Time: 22 ms (89.29%), Space: 56.5 MB (37.79%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,32 +1,32 @@
 class Solution {
+    Boolean[][] memo;
     public boolean canPartition(int[] nums) {
-        int sum = 0;
-        for(int i : nums) {
-            sum += i;
+        int totalSum = 0;
+
+        for (int num : nums) {
+            totalSum += num;
+        }
+        if (totalSum%2 != 0) return false;
+
+        memo = new Boolean[nums.length][totalSum / 2 + 1];
+
+        return dfs (nums, 0, totalSum/2);
+
+    }
+
+    boolean dfs (int[] nums, int index, int target) {
+        if (index == nums.length) {
+            return target == 0;
         }
 
-        if (sum%2 != 0) {
-            return false;
-        }
-        
-        sum /= 2;
+        if (target < 0) return false;
 
-        Set<Integer> cache = new HashSet<>();
-        cache.add(0);
-
-        for (int i : nums) {
-            Set<Integer> tempCache = new HashSet<>();
-
-            for (int c : cache) {
-                int addition = c + i;
-                if (addition == sum) {
-                    return true;
-                }
-                tempCache.add(addition);
-            }
-            cache.addAll(tempCache);
+        if (memo[index][target] != null) {
+            return memo[index][target];
         }
 
-        return false;
+        memo[index][target] = dfs (nums, index+1, target - nums[index]) || dfs (nums, index+1, target);
+
+        return memo[index][target];
     }
 }
