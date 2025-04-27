35c604de9a2dd4087127eeca3a1a5b4603c32158
Rishit Pallav <64432673+rishitpallav@users.noreply.github.com>
2025-04-27 14:51:26 -0500

    Time: 5 ms (75.94%), Space: 42.4 MB (42.61%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.java
+++ 0768-partition-labels/0768-partition-labels.java
@@ -1,24 +1,31 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
+        int[] frequencies = new int[26];
+
+        for (char c : s.toCharArray()) {
+            frequencies[c - 'a']++;
+        }
+
         List<Integer> result = new ArrayList<>();
+        int left = -1, charCount = 0, fullMatch = 0;
+        int[] currentFreq = new int[26];
 
-        int[] lastIndex = new int[26];
-        
-        for (int i = 0; i < s.length(); i++ ) {
-            lastIndex[s.charAt(i) - 'a'] = i;
-        }
-
-        int startIndex = 0, farIndex = 0;
-        for (int i = 0; i < s.length(); i++ ) {
-            if (farIndex < i) {
-                result.add(farIndex - startIndex + 1);
-                startIndex = i;
-                farIndex = i;
+        for (int right = 0; right < s.length(); right++ ) {
+            char c = s.charAt(right);
+            currentFreq[c - 'a']++;
+            if (currentFreq[c - 'a'] == 1) {
+                charCount++;
             }
-            farIndex = Math.max(farIndex, lastIndex[s.charAt(i) - 'a']);
+            if (currentFreq[c-'a'] == frequencies[c-'a']) {
+                fullMatch++;
+            }
+            if (fullMatch == charCount) {
+                result.add(right - left);
+                fullMatch = 0;
+                charCount = 0;
+                left = right;
+            }
         }
-
-        result.add(s.length() - startIndex);
 
         return result;
     }
