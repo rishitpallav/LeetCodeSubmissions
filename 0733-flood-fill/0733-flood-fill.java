99732955899a21b360baf3f63b96bbfbc3dbd2d7
Rishit Pallav <64432673+rishitpallav@users.noreply.github.com>
2025-04-26 23:21:10 -0500

    Time: 1 ms (47.47%), Space: 45.6 MB (5.34%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,28 +1,34 @@
 class Solution {
+
+    class Pair {
+        int a;
+        int b;
+        Pair(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+    }
+
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
 
-        if (image[sr][sc] == color) {
-            return image;
-        }
+        if (image[sr][sc] == color) return image;
 
-        Queue<int[]> connectedCells = new LinkedList<>();
-        connectedCells.add(new int[]{sr,sc});
+        Queue<Pair> bfs = new LinkedList<>();
+        int previousColor = image[sr][sc];
 
-        int[][] directions = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
-        int n = image.length;
-        int m = image[0].length;
+        bfs.add(new Pair(sr, sc));
+        int[][] directions = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+        int n = image.length, m = image[0].length;
+        image[sr][sc] = color;
 
-        int sourceColor = image[sr][sc];
+        while (!bfs.isEmpty()) {
+            Pair currentPair = bfs.poll();
 
-        while (!connectedCells.isEmpty()) {
-            int[] cell = connectedCells.poll();
-            image[cell[0]][cell[1]] = color;
             for (int[] d : directions) {
-                int i = cell[0] + d[0];
-                int j = cell[1] + d[1];
-
-                if (i >= 0 && j >= 0 && i < n && j < m && image[i][j] == sourceColor) {
-                    connectedCells.add(new int[]{i,j});
+                int new_x = d[0] + currentPair.a, new_y = d[1] + currentPair.b;
+                if (new_x >= 0 && new_x < n && new_y >= 0 && new_y < m && image[new_x][new_y] == previousColor) {
+                    image[new_x][new_y] = color;
+                    bfs.add(new Pair(new_x, new_y));
                 }
             }
         }
