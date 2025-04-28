2ec9d27fb06c3c809acfbffc4000d49f827ff5d9
Rishit Pallav <64432673+rishitpallav@users.noreply.github.com>
2025-04-28 11:12:30 -0500

    Time: 1 ms (100%), Space: 46.4 MB (26.27%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,58 +1,19 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int left = 0, right = 0, n = nums1.length, m = nums2.length;
-        boolean isCurrentLeft = true;
-        if (left >= n || (right < m && nums1[left] > nums2[right])) {
-            isCurrentLeft = false;
-        }
-
         int counter = (n + m)  / 2;
-        if ((n + m)%2 == 0) {
+        int prev = 0, current = 0;
+
+        while (counter >= 0) {
+            prev = current;
+            if ((left < n) && (right >= m || nums1[left] < nums2[right])) {
+                current = nums1[left++];
+            } else {
+                current = nums2[right++];
+            }
             counter--;
         }
 
-        while (counter > 0) {
-            if (isCurrentLeft) {
-                left++;
-            } else {
-                right++;
-            }
-            counter--;
-            if (left < n) {
-                if (right >= m || nums1[left] < nums2[right]) {
-                    isCurrentLeft = true;
-                } else {
-                    isCurrentLeft = false;
-                }
-            } else {
-                isCurrentLeft = false;
-            }
-        }
-
-        if ((n+m)%2!=0) {
-            return (double) ((isCurrentLeft)?nums1[left]:nums2[right]);
-        } else {
-            double result = 0.0;
-            int smallest = 0;
-            int nextSmallest = 0;
-            if (isCurrentLeft) {
-                smallest = nums1[left];
-                left++;
-            } else {
-                smallest = nums2[right];
-                right++;
-            }
-            if (left < n) {
-                if (right >= m || nums1[left] < nums2[right]) {
-                    nextSmallest = nums1[left];
-                } else {
-                    nextSmallest = nums2[right];
-                }
-            } else {
-                nextSmallest = nums2[right];
-            }
-
-            return ((double) (smallest + nextSmallest))/2;
-        }
+        return ((n+m)%2==0) ? (prev + current)/2.0 : current;
     }
 }
