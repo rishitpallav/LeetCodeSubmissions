03ffdb06670097812c6bf51587b3e356574c9dd9
Rishit Pallav <64432673+rishitpallav@users.noreply.github.com>
2025-05-03 11:51:58 -0500

    Time: 0 ms (100%), Space: 41.5 MB (56.39%) - LeetHub

--- 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.java
+++ 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.java
@@ -14,7 +14,7 @@
  * }
  */
 class Solution {
-    int xIndex = -1, yIndex = -1;
+    int xIndex = Integer.MAX_VALUE, yIndex = Integer.MAX_VALUE;
     TreeNode xParent = null, yParent = null;
     public boolean isCousins(TreeNode root, int x, int y) {
         dfs (root, 0, x, y, null);
@@ -22,17 +22,19 @@ class Solution {
     }
 
     void dfs (TreeNode root, int currentDepth, int x, int y, TreeNode previous) {
-        if (root == null || (xIndex != -1 && yIndex != -1)) {
+        if (root == null || (xIndex != Integer.MAX_VALUE && yIndex != Integer.MAX_VALUE) || (currentDepth > xIndex) || (currentDepth > yIndex)) {
             return;
         }
 
         if (root.val == x) {
             xIndex = currentDepth;
             xParent = previous;
+            return;
         }
         if (root.val == y) {
             yIndex = currentDepth;
             yParent = previous;
+            return;
         }
 
         dfs (root.left, currentDepth + 1, x, y, root);
