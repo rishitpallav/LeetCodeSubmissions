29ecb109b8eecaf361a12e2bd58dac39bed1fa3c
Rishit Pallav <64432673+rishitpallav@users.noreply.github.com>
2025-05-11 15:10:24 -0500

    Time: 7 ms (100%), Space: 44.3 MB (85.45%) - LeetHub

--- 3314-most-frequent-prime/3314-most-frequent-prime.java
+++ 3314-most-frequent-prime/3314-most-frequent-prime.java
@@ -5,7 +5,6 @@ class Solution {
         for (int i = 0; i < mat.length; i++ ) {
             for (int j = 0; j < mat[0].length; j++ ) {
                 getAllNumbers(mat, i, j);
-                System.out.println(mostFreq);
             }
         }
 
