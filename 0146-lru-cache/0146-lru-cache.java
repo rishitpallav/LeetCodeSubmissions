8ee9d8bbc3bf1446e6d774c3826c091af46b98dd
Rishit Pallav <64432673+rishitpallav@users.noreply.github.com>
2025-05-15 23:19:53 -0500

    Time: 52 ms (25.63%), Space: 114.9 MB (51.12%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,21 +1,79 @@
+class ListNode {
+    int key;
+    int value;
+    ListNode prev;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
 class LRUCache {
 
-    LinkedHashMap<Integer, Integer> database;
+    int capacity;
+    HashMap<Integer, ListNode> cache = new HashMap<>();
+    ListNode mostRecent = new ListNode(-1,-1);
+    ListNode leastRecent = new ListNode(-1,-1);
 
     public LRUCache(int capacity) {
-        database = new LinkedHashMap<>(capacity, 0.75f, true) {
-            protected boolean removeEldestEntry (Map.Entry<Integer, Integer> eldest) {
-                return size() > capacity;
-            }
-        };
+        this.capacity = capacity;
+        mostRecent.next = leastRecent;
+        leastRecent.next = mostRecent;
+    }
+
+    public void addNode(ListNode newNode) {
+        ListNode temp = mostRecent.next;
+        
+        newNode.next = temp;
+        newNode.prev = mostRecent;
+        
+        mostRecent.next = newNode;
+        temp.prev = newNode;
+    }
+
+    public void deleteNode(ListNode node) {
+        ListNode prevNode = node.prev;
+        ListNode nextNode = node.next;
+
+        nextNode.prev = prevNode;
+        prevNode.next = nextNode;
     }
     
     public int get(int key) {
-        return database.getOrDefault(key, -1);
+        if (cache.containsKey(key)) {
+
+            ListNode resNode = cache.get(key);
+            int result = resNode.value;
+
+            cache.remove(key);
+            deleteNode(resNode);
+            addNode(resNode);
+
+            cache.put(key, mostRecent.next);
+
+            return result;
+        }
+        return -1;
     }
     
     public void put(int key, int value) {
-        database.put(key, value);
+        if (cache.containsKey(key)) {
+            ListNode current = cache.get(key);
+            cache.remove(key);
+            deleteNode(current);
+        }
+
+        if (cache.size() == capacity) {
+            cache.remove(leastRecent.prev.key);
+            deleteNode(leastRecent.prev);
+        }
+
+        addNode(new ListNode(key, value));
+        cache.put(key, mostRecent.next);
     }
 }
 
