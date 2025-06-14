b586215b0f851c6d7fc8776528c835d0699848f0
Rishit Pallav <64432673+rishitpallav@users.noreply.github.com>
2025-06-13 21:24:33 -0500

    Time: 0 ms (100%), Space: 40.2 MB (97.73%) - LeetHub

--- 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.java
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.java
@@ -1,32 +1,30 @@
 class Solution {
     public int minMaxDifference(int num) {
-        char large = ' ';
-        char small = ' ';
+        int large = -1;
+        int small = -1;
 
-        String value = String.valueOf(num);
+        int value = num;
 
-        for (int i = 0; i < value.length() && (large == ' ' || small == ' '); i++ ) {
-            if (large == ' ' && value.charAt(i) != '9') {
-                large = value.charAt(i);
-            }
-            if (small == ' ' && value.charAt(i) != '0') {
-                small = value.charAt(i);
-            }
+        while (value > 0) {
+            int temp = value % 10;
+            value /= 10;
+            if (temp != 9) large = temp;
+            if (temp != 0) small =  temp;
         }
 
+        value = num;
         int result = 0;
-        for (int i = 0; i < value.length(); i++ ) {
-            char c = value.charAt(i);
-            if (c == small) {
-                result *= 10;
-                result += 9;
-            } else {
-                if (c == large) {
-                    result *= 10;
-                    result += '9' - c;
-                } else {
-                    result *= 10;
-                }
+        int counter = -1;
+
+        while (value > 0) {
+            int temp = value % 10;
+            value /= 10;
+            counter++;
+
+            if (temp == small) {
+                result += 9 * Math.pow(10, counter);
+            } else if (temp == large) {
+                result += (9 - temp) * Math.pow(10, counter);
             }
         }
 
